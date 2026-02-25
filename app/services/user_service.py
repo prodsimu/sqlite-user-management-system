c871b2c186e1ce416fb22ff5138e2ba11f86b74e
simu-T14 <139299982+prodsimu@users.noreply.github.com>
2026-02-25 12:41:48 -0300

    feat: add delete_user method to UserService

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -11,6 +11,8 @@ class UserService:
     def __init__(self):
         self.user_repository = UserRepository()
 
+    # CREATE
+
     def create_user(self, name: str, username: str, password: str) -> User:
         existing_user = self.user_repository.find_by_username(username)
 
@@ -21,6 +23,8 @@ class UserService:
 
         return self.user_repository.create(name, username, hashed_password)
 
+    # READ
+
     def get_user_by_username(self, username: str) -> User:
         user = self.user_repository.find_by_username(username)
 
@@ -28,6 +32,18 @@ class UserService:
             raise UserNotFoundError("User not found.")
 
         return user
+
+    # UPDATE
+
+    # DELETE
+
+    def delete_user(self, user_id: int) -> None:
+        deleted = self.user_repository.delete(user_id)
+
+        if not deleted:
+            raise UserNotFoundError("User not found.")
+
+    # AUTH
 
     def authenticate_user(self, username: str, password: str) -> User:
         user = self.user_repository.find_by_username(username)
