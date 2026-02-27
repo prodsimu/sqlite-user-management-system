7af153502de5e6a7633b7a691e50f5254bda41c8
simu-T14 <139299982+prodsimu@users.noreply.github.com>
2026-02-27 19:38:16 -0300

    feat: implement create session method to session service

--- app/services/session_service.py
+++ app/services/session_service.py
@@ -6,3 +6,11 @@ class SessionService:
 
     def __init__(self, session_repository: SessionRepository) -> None:
         self.session_repository: SessionRepository = session_repository
+
+    def create_session(self, user_id: int) -> Session:
+        active_session = self.session_repository.get_active_by_user(user_id)
+
+        if active_session:
+            self.session_repository.deactivate(active_session.id)
+
+        return self.session_repository.create(user_id)
