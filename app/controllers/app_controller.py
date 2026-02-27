33e5fd84430138cad9872d3f2745b019e6d59242
simu-T14 <139299982+prodsimu@users.noreply.github.com>
2026-02-27 20:37:21 -0300

    feat: auto-login seeded admin user on startup

--- app/controllers/app_controller.py
+++ app/controllers/app_controller.py
@@ -1,14 +1,27 @@
 from app.services.user_service import UserService
+from app.services.session_service import SessionService
 from app.database.seeds import admin_seed
 
 
 class AppController:
-    def __init__(self, user_service: UserService) -> None:
-        self.user_service: UserService = user_service
+    def __init__(
+        self,
+        user_service: UserService,
+        session_service: SessionService,
+    ) -> None:
+        self.user_service = user_service
+        self.session_service = session_service
+        self.current_user = None
         self.current_session = None
 
     def bootstrap(self) -> None:
-        seed = admin_seed(self.user_service)
+        admin_seed(self.user_service)
+
+        admin = self.user_service.get_user_by_username("admin")
+        session = self.session_service.create(admin.id)
+
+        self.current_user = admin
+        self.current_session = session
 
     def start_app(self) -> None:
         self.bootstrap()
