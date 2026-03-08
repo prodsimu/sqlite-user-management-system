e435538fd4d1efffe904481b43aa6f45c1835f78
prodsimu <139299982+prodsimu@users.noreply.github.com>
2026-03-08 19:03:27 -0300

    test: add UserRepository find_by_id test

--- tests/repositories/test_user_repository.py
+++ tests/repositories/test_user_repository.py
@@ -2,3 +2,11 @@ def test_user_repository_create(user_repository):
     user = user_repository.create("ignatius", "ignatius123", "hashed_password", "user")
 
     assert user.id == 1
+
+
+def test_user_repository_find_by_id(user_repository):
+    user_repository.create("ignatius", "ignatius123", "hashed_password", "user")
+
+    user = user_repository.find_by_id(1)
+
+    assert user.username == "ignatius123"
