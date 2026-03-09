6a683be527c2ea23db7058230beeeab9dea307c4
simu-T14 <139299982+prodsimu@users.noreply.github.com>
2026-03-09 07:55:36 -0300

    test: add SessionRepository get_active_by_user test

--- tests/repositories/test_session_repository.py
+++ tests/repositories/test_session_repository.py
@@ -20,3 +20,16 @@ def test_session_repository_deactivate(user_repository, session_repository):
     session_inactive = session_repository.get_active_by_user(user.id)
 
     assert session_inactive is None
+
+
+def test_session_repository_get_active_by_user(user_repository, session_repository):
+    user = user_repository.create("ignatius", "ignatius123", "hashed_password", "user")
+
+    session = session_repository.create(user.id)
+
+    active_session = session_repository.get_active_by_user(user.id)
+
+    assert active_session is not None
+    assert active_session.id == session.id
+    assert active_session.user_id == user.id
+    assert active_session.active == SessionActive.ACTIVE.value
