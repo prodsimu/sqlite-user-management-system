2442fb091a323dceba278bc4d72d3c46afaf8a14
simu-T14 <139299982+prodsimu@users.noreply.github.com>
2026-03-09 08:02:24 -0300

    test: add UserService create_admin test

--- tests/services/test_user_service.py
+++ tests/services/test_user_service.py
@@ -9,3 +9,13 @@ def test_user_service_create(user_service):
     assert user.username == "ignatius123"
     assert user.role == UserRole.USER.value
     assert user.password != "password123"
+
+
+def test_user_service_create_admin(user_service):
+    admin = user_service.create_admin("Admin", "admin", "admin123")
+
+    assert admin.id is not None
+    assert admin.name == "Admin"
+    assert admin.username == "admin"
+    assert admin.role == UserRole.ADMIN.value
+    assert admin.password != "admin123"
